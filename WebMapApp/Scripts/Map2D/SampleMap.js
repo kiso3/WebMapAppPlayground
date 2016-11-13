22fd3e77bdf94e3cca2cb5a1b3a7b9af2ab60e22
kiso3 <kiso33@gmail.com>
2016-11-13 12:24:41 +0100

    20161113

---
+++ WebMapApp/Scripts/Map2D/SampleMap.js
@@ -0,0 +1,8 @@
+﻿var map = new OpenLayers.Map("map");
+
+var opl_wms = new OpenLayers.Layer.WMS("test"
+    ,"http://vmap0.tiles.osgeo.org/wms/vmap0"
+    ,{layers: "basic"}
+);
+
+map.addLayer(opl_wms);
---
+++ WebMapApp/Scripts/_references.js
@@ -0,0 +1,4 @@
+﻿/// <reference path="OpenLayers-2.13.1/OpenLayers.light.debug.js" />
+/// <reference path="Ext-3.4.1/adapter/ext/ext-base-debug.js" />
+/// <reference path="Ext-3.4.1/ext-all-debug.js" />
+/// <reference path="GeoExt-1.1/GeoExt.js" />
